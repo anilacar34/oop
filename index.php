e39bbdf05009b637aab0ca39db20376d239d63b2
Anıl Çağatay ACAR <acbold@msn.com>
2021-03-14 23:33:28 +0300

    updated object name to lowercase.

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 require_once "Fridge.php";
 
 $product = 'coke';
-$Fridge = new Fridge();
+$fridge = new Fridge();
 
 for($shelf = 1; $shelf < 4; $shelf++){
-    $Fridge->deleteProduct($shelf,$product);  // An error message is displayed after trying to remove a coke from an empty shelf.
+    $fridge->deleteProduct($shelf,$product);  // An error message is displayed after trying to remove a coke from an empty shelf.
     for($i = 0; $i < 21; $i++){
-        $Fridge->addProduct($shelf,$product); // Putting an coke to the empty/partial shelf. (20 times)
+        $fridge->addProduct($shelf,$product); // Putting an coke to the empty/partial shelf. (20 times)
     }
-    $Fridge->deleteProduct($shelf,$product); // Remove a coke from a shelf.
+    $fridge->deleteProduct($shelf,$product); // Remove a coke from a shelf.
 }
 
-$Fridge->print();
+$fridge->print();
 
 //print_r($Fridge->getShelvesData()); // raw data print
 
